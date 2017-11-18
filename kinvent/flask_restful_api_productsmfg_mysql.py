c34425b7d4ffa4fe8aa3322df4e987fe0b1e3045
kiran5july <kiran.learn7@gmail.com>
2017-11-18 12:03:08 -0700

    Update flask_restful_api_productsmfg_mysql.py

--- kinvent/flask_restful_api_productsmfg_mysql.py
+++ kinvent/flask_restful_api_productsmfg_mysql.py
@@ -1,3 +1,9 @@
+
+#RESTful service
+#Start the service: python flask_restful_api_productsmfg_mysql.py
+#Test using an API test tool ( Ex: PostMan, SoapUI)
+
+
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
