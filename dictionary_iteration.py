d57746f6703537bf9153e355bee4dd3b478bdb12
km <kiran.learn7@gmail.com>
2021-07-05 19:05:31 -0500

    Dict ops

--- dictionary_iteration.py
+++ dictionary_iteration.py
@@ -4,37 +4,25 @@ print("------for loop by keys")
 for k in d:
     print(k, '--->', d[k])
 
-'''
-x ---> {'x': 1, 'z': 3, 'y': 2}
-z ---> {'x': 1, 'z': 3, 'y': 2}
-y ---> {'x': 1, 'z': 3, 'y': 2}
-'''
-
 
 print("------items(): reads key & val as tuple")
 for k in d.items():
-    print(k)
-'''
-('z', 3) ---> {'z': 3, 'y': 2, 'x': 1}
-('y', 2) ---> {'z': 3, 'y': 2, 'x': 1}
-('x', 1) ---> {'z': 3, 'y': 2, 'x': 1}
-'''
+    print(k, ' --> ', k[0], ' -- ', k[1])
 
 print("------items (read key & value) ")
 for k, v in d.items():
     print(k, '--->', v)
-'''
-x ---> 1
-y ---> 2
-z ---> 3
-'''
+
+print("------by keys ")
+for k in d.keys():
+    print(k, '--->', d[k])
+
+#for k in sorted(d.keys()):
+#    print(k, '--->', d[k])
 
 
+print("------by values ")
+for v in d.values():
+    print(v)
 
-'''
-print("------iteritems()")
-#This is deprecated in Python3 & replaced by items() 
-for k in d.iteritems():
-    print (k, '--->', d)
 
-'''
