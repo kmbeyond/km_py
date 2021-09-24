d8365a82486566be8847c7e456c12e96f16df543
km <kiran.learn7@gmail.com>
2021-09-24 15:25:39 -0500

    boto3 s3

--- aws_boto3_s3_access.py
+++ aws_boto3_s3_access.py
@@ -2,6 +2,7 @@
 
 import boto3
 
+#km1
 access_key = 'AKIAVP2HFGCVOUOKINV6'
 secret_key = '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'
 
@@ -11,6 +12,7 @@ conn_s3 = boto3.resource(
     aws_access_key_id=access_key,
     aws_secret_access_key=secret_key
 )
+
 
 for bucket in conn_s3.buckets.all():
     print(bucket.name)
