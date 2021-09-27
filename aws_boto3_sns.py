3089de2ca84c46f904987d3e62fb60e02e3546e5
kmbeyond <kiran.learn7@gmail.com>
2021-09-27 18:15:51 -0400

    Create aws_boto3_sns.py

---
+++ aws_boto3_sns.py
@@ -0,0 +1,40 @@
+import boto3
+from botocore.config import Config
+import logging
+
+# logging setup
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def send_sns_message(account_num, region_name, topic_name, subject, message):
+    #km1
+    access_key = ''
+    secret_key = ''
+    topic_arn='arn:aws:sns:'+region_name+':'+account_num+':'+topic_name
+    config={
+        "sns_arn": topic_arn
+    }
+    sns = boto3.resource('sns',
+        endpoint_url='https://sns.'+region_name+'.amazonaws.com',
+        region_name=region_name,
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key
+    )
+    # publish to SNS
+    response = topic.publish(
+        Subject=subject,
+        Message=json.dumps({'default': message}),
+        MessageStructure='json'
+    )
+
+
+def main():
+    region_name='us-east-1'
+    topic_name=''
+    account_num=''
+    subject = "test subject"
+    message = '{0}'.format("simple test message")
+    send_sns_message(account_num,region_name, topic_name, subject, message)
+
+if __name__ == "__main__":
+    main()
