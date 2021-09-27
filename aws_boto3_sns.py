3faaae034fe694b91f2e07abc1f726dd23f05c32
km <kiran.learn7@gmail.com>
2021-09-27 17:39:59 -0500

    boto3 SNS publish message

--- aws_boto3_sns.py
+++ aws_boto3_sns.py
@@ -1,7 +1,10 @@
 import boto3
 from botocore.config import Config
 import logging
+import json
 
+# SETUP:
+#User must be authorized to use SNS - add
 # logging setup
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -11,7 +14,7 @@ def send_sns_message(account_num, region_name, topic_name, subject, message):
     access_key = ''
     secret_key = ''
     topic_arn='arn:aws:sns:'+region_name+':'+account_num+':'+topic_name
-    config={
+    config = {
         "sns_arn": topic_arn
     }
     sns = boto3.resource('sns',
@@ -20,6 +23,7 @@ def send_sns_message(account_num, region_name, topic_name, subject, message):
         aws_access_key_id=access_key,
         aws_secret_access_key=secret_key
     )
+    topic = sns.Topic(config['sns_arn'])
     # publish to SNS
     response = topic.publish(
         Subject=subject,
@@ -29,12 +33,12 @@ def send_sns_message(account_num, region_name, topic_name, subject, message):
 
 
 def main():
-    region_name='us-east-1'
-    topic_name=''
-    account_num=''
+    region_name = 'us-east-1'
+    topic_name = ''
+    account_num = ''
     subject = "test subject"
     message = '{0}'.format("simple test message")
-    send_sns_message(account_num,region_name, topic_name, subject, message)
+    send_sns_message(account_num, region_name, topic_name, subject, message)
 
 if __name__ == "__main__":
     main()
--- ops_strings.py
+++ ops_strings.py
@@ -1,9 +1,17 @@
 
 
-sName="Hello World"
+str1="Hello World"
 
-print(sName)
+print(str1)
 
-print("Length: {}".format(len(sName)))
+print("Length: {}".format(len(str1)))
 
-print("Length: {}".format(sName[:-3]))
+print("Length: {}".format(str1[:-3]))
+
+strings = str1.split()
+print("{0}\n{1}".format(strings, type(strings)))
+
+s_new_line="""
+"""
+new_string = "line1 line2 line3".replace(' ',s_new_line)
+print(new_string)
