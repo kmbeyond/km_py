db195311d8ee96b9acb2a7e143eaee9f4bcd5d4b
kmbeyond <kiran.learn7@gmail.com>
2022-01-03 17:59:37 -0500

    Update aws_boto3_secrets.py

--- aws_boto3_secrets.py
+++ aws_boto3_secrets.py
@@ -7,6 +7,13 @@ import boto3, json
 sm_key="km_key"
 sm_client = boto3.client(service_name="secretsmanager", region_name="us-east-1")
 conf_val = sm_client.get_secret_value(SecretId="km_conf")['SecretString']
-d_conf_val = json.loads(conf_val)
-conf_list = d_conf_val['km_conf'].split('<x>')
+conf_list = conf_val.split('<x>')
+
 km_conf = {'k1':conf_list[0], 'k2':conf_list[1], 'k3':conf_list[2], 'k4':conf_list[3],'k5':conf_list[4]}
+
+
+#value as json string
+#value: {"k1":"abcd","k2":"efgh","k3":"ijkl","k4":"mnop","k5":"qrst"}
+conf_val = sm_client.get_secret_value(SecretId="km_conf")['SecretString']
+d_conf_val = json.loads(conf_val)
+k1=d_conf_val["k1"]
