8a0648d43f26e2cca1c5b47e7becff4aa0678a1f
kmbeyond <kiran.learn7@gmail.com>
2022-01-21 14:02:22 -0500

    Update dag_km_ses.py

--- af/dag_km_ses.py
+++ af/dag_km_ses.py
@@ -1,35 +1,30 @@
 from datetime import datetime, timedelta
-import logging
+import boto3, logging
 import airflow.hooks.S3_hook
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-#from airflow.contrib.hooks.snowflake_hook import SnowflakeHook
-from airflow.operators.bash_operator import BashOperator
-#from airflow.operators.dummy_operator import DummyOperator
-#from airflow.operators.email_operator import EmailOperator
-import boto3
 from botocore.config import Config
 
 # logging setup
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-aws_account=""
 
+to_email_address = "km@gmail.com"
+ses_source_email = "km@gmail.com"
 
-def send_html_email(subject, body):
-    print("aws_account:" + aws_account)
+def send_html_email(ses_source_email, to_email_address, subject, body):
+    #logger.info("aws_account:" + aws_account)
     config_ses = Config(
         retries={
             'max_attempts': 2,
             'mode': 'standard'
         }
     )
-    ses_client = boto3.client("ses", region_name="us-east-1",
-                              config=config_ses)
+    ses_client = boto3.client("ses", region_name="us-east-1", config=config_ses)
     #endpoint_url="email-smtp.us-east-1.amazonaws.com",
     response = ses_client.send_email(
         Destination={
-            "ToAddresses": ["km@gmail.com"],
+            "ToAddresses": [to_email_address],
         },
         Message={
             "Body": {
@@ -43,49 +38,52 @@ def send_html_email(subject, body):
                 "Data": subject,
             },
         },
-        Source='km@gmail.com'
+        Source=ses_source_email
     )
 
 
-def ppa_send_email():
-    #result = str(snowflake_hook_autocommit.get_first(f"CALL MY_STORED_PROC()"))
+def send_ses_email(to_email_address, ses_source_email, **kwargs):
+    """Calls snowflake stored procedure to get Monthly performance metrics into string"""
+    #result = str(snowflake_hook_autocommit.get_first(f"CALL METRICS_REPORT_MONTHLY()"))
     result = "<h3>welcome to SES email</h3>"
     try:
-        send_html_email("SES test", result)
-        print("SES Success")
+        send_html_email(ses_source_email, to_email_address, "SES test", result)
+        logger.info("SES Success")
     except:
-        print("Failed to send using SES")
+        logger.info("Failed to send using SES")
     else:
-        print('Email sent! Message ID:')
+        logger.info('Email sent! Message ID:')
         #print(response['MessageId'])
+
+def dag_success_notification(**kwargs):
+    logger.info("DAG Success")
+
+
+def dag_failure_notification(**kwargs):
+    logger.info("DAG Failed")
+    logger.error("DAG Failed")
+
 
 default_args = {
     'owner': 'airflow',
     'start_date': airflow.utils.dates.days_ago(2),
     'retries': 3,
     'retry_delay': timedelta(minutes=0),
-    'on_failure_callback': '',
-    'on_success_callback': ''
+    'on_failure_callback': dag_failure_notification,
+    'on_success_callback': dag_success_notification
 }
 
-with DAG('dag_km_ses', default_args=default_args, schedule_interval='0 12 1 * *', catchup=False,
-         dagrun_timeout=timedelta(minutes=90)) as dag:
-
-    bash_task = BashOperator(
-        task_id='bash_task',
-        bash_command="""echo "Welcome Let's start..." """
-    )
+with DAG('dag_km_ses', default_args=default_args, schedule_interval=None, catchup=False, dagrun_timeout=timedelta(minutes=90)) as dag:
 
     email_task = PythonOperator(
         task_id='email_task',
-        python_callable=ppa_send_email
+        python_callable=send_ses_email,
+        provide_context=True,
+        op_kwargs={
+            'to_email_address': to_email_address,
+            'ses_source_email': ses_source_email
+        }
     )
-    bash_task >> email_task
+    email_task
 
-    #email_task = EmailOperator(
-    #    task_id='email_task',
-    #    to='km@gmail.com',
-    #    subject='Airflow Alert',
-    #   html_content=""" <h3>Email Test</h3> """,
-    #)
-    #execute_metrics_report_monthly_task >> email_task
+
