2cbf45fdcc34c98f8176760273486b10678ca45f
km <kiran.learn7@gmail.com>
2022-02-07 17:38:33 -0600

    km

--- aws_boto3_sns.py
+++ aws_boto3_sns.py
@@ -11,20 +11,14 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def send_sns_message(account_num, region_name, topic_name, subject, message):
-    #km1
-    access_key = ''
-    secret_key = ''
     topic_arn='arn:aws:sns:'+region_name+':'+account_num+':'+topic_name
-    config = {
-        "sns_arn": topic_arn
-    }
     sns = boto3.resource('sns',
         endpoint_url='https://sns.'+region_name+'.amazonaws.com',
-        region_name=region_name,
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key
+        region_name=region_name
+        #aws_access_key_id=access_key,
+        #aws_secret_access_key=secret_key
     )
-    topic = sns.Topic(config['sns_arn'])
+    topic = sns.Topic(topic_arn)
     # publish to SNS
     #response = topic.publish(
     #    Subject=subject,
