81b6247fdc69cb799608291937dedbdb618e3e80
kmbeyond <kiran.learn7@gmail.com>
2022-02-12 19:19:41 -0500

    Update aws_km_lambda_process_s3_notification.py

--- aws_km_lambda_process_s3_notification.py
+++ aws_km_lambda_process_s3_notification.py
@@ -21,7 +21,7 @@ Policy "km-policy-access-to-s3":
 }
 
 Event=
-{"Records": [{"eventVersion": "2.1", "eventSource": "aws:s3", "awsRegion": "us-east-1", "eventTime": "2022-02-08T00:18:41.846Z", "eventName": "ObjectCreated:Put", "userIdentity": {"principalId": "AWS:xxx"}, "requestParameters": {"sourceIPAddress": "x.x.x.x"}, "responseElements": {"x-amz-request-id": "xxx", "x-amz-id-2": "xxxx"}, "s3": {"s3SchemaVersion": "1.0", "configurationId": "lambda_process_file", "bucket": {"name": "km-test-bucket", "ownerIdentity": {"principalId": "xxx"}, "arn": "arn:aws:s3:::km-test-bucket"}, "object": {"key": "km_lambda/test_data_2022-01-28.csv", "size": 64, "eTag": "xxx", "sequencer": "006201B6E1CC1BA63D"}}}]}
+{"Records": [{"eventVersion": "2.1", "eventSource": "aws:s3", "awsRegion": "us-east-1", "eventTime": "2022-02-08T00:18:41.846Z", "eventName": "ObjectCreated:Put", "userIdentity": {"principalId": "AWS:xxx"}, "requestParameters": {"sourceIPAddress": "x.x.x.x"}, "responseElements": {"x-amz-request-id": "xxx", "x-amz-id-2": "xxxx"}, "s3": {"s3SchemaVersion": "1.0", "configurationId": "lambda_process_file", "bucket": {"name": "km-test-bucket", "ownerIdentity": {"principalId": "xxx"}, "arn": "arn:aws:s3:::km-test-bucket"}, "object": {"key": "km_ingestion/test_data_2022-01-28.csv", "size": 64, "eTag": "xxx", "sequencer": "006201B6E1CC1BA63D"}}}]}
 
 
 '''
@@ -31,7 +31,7 @@ import json, boto3,os
 def lambda_handler(event, context):
     print(f"Event: {event}")
     print(f"context: {context}")
-    #bucket_name="km-test-bucket-ppa"
+    #bucket_name="km-bkt"
     bucket_name = event.get("Records")[0].get("s3").get("bucket").get("name")
     file_name_w_prefix = event.get("Records")[0].get("s3").get("object").get("key")
     print(f"Event from: s3://{bucket_name}/{file_name_w_prefix}")
