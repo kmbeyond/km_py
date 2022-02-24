2004ea1b99c3ed51346ba67528f7b572086e3348
kmbeyond <kiran.learn7@gmail.com>
2022-02-24 18:27:39 -0500

    Update loop_iteration_logic.py

--- loop_iteration_logic.py
+++ loop_iteration_logic.py
@@ -3,47 +3,76 @@ from datetime import datetime, date, timedelta
 def getDT():
  return datetime.now().strftime( '%Y-%m-%d %H:%M:%S')
 
-days_total=int(50)
-increments=int(7)
 
-#----Ascend: Low to High
-lowLimit=6
-upperLimit=lowLimit+increments
+days_total = 50
+increments = 5
+
+# ----Ascend: Low to High
+print("------ Low to High ------")
+lowLimit = 6
+upperLimit = lowLimit + increments -1
 
 while lowLimit <= days_total:
- upperLimit = days_total if upperLimit>days_total else upperLimit
- print("{}: range: {} - {}".format(getDT(), lowLimit, upperLimit) )
- #Other operations
- 
- lowLimit=upperLimit+1
- upperLimit=lowLimit+increments
- 
+    upperLimit = days_total if upperLimit > days_total else upperLimit
+    print(f"{getDT()}: Range: {lowLimit} - {upperLimit}")
+    # Other operations
 
- #----Descend: High to Low
-upperLimit=days_total
-lowLimit=upperLimit-increments
+    lowLimit = upperLimit + 1
+    upperLimit = upperLimit + increments
+
+'''#Similar
+#start_count = 1
+end_count = 100
+
+loop_start = 21
+increments = 5
+loop_end = loop_start+increments -1
+#loop_end = 25
+
+while (loop_start <= end_count):
+    loop_start = end_count if loop_start > end_count else loop_start
+    loop_end = end_count if loop_end > end_count else loop_end
+    print(getDT() + ": Loop for: " + str(loop_start) + " -> " + str(loop_end))
+
+
+    loop_start = loop_end + 1
+    loop_end = loop_end + increments
+
+'''
+
+# ----Descend: High to Low
+print("------ High to Low ------")
+days_total = 50
+decrements = 5
+
+upperLimit = days_total
+lowLimit = upperLimit - decrements + 1
 
 while upperLimit >= 1:
- lowLimit=1 if lowLimit<1 else lowLimit
- print("{}: Days range: {} - {}".format(getDT(), lowLimit, upperLimit) )
- 
- upperLimit=lowLimit-1
- lowLimit=upperLimit-increments
+    lowLimit = 1 if lowLimit < 1 else lowLimit
+    print(f"{getDT()}: Range: {upperLimit} - {lowLimit}")
 
- 
- #----iterate/loop between dates (old date to current)
-increments=int(30)
-sDateFrom='2020-02-05'
+    upperLimit = lowLimit - 1
+    lowLimit = lowLimit - decrements
 
-lowLimit=1
-upperLimit=lowLimit+increments
-s_dt_from = (date.today()-timedelta(upperLimit)).strftime('%Y-%m-%d')
 
-while s_dt_from > sDateFrom:
- s_dt_from = (date.today()-timedelta(upperLimit)).strftime('%Y-%m-%d')
- s_dt_to = (date.today()-timedelta(lowLimit)).strftime('%Y-%m-%d')
- s_dt_from = sDateFrom if sDateFrom>s_dt_from else s_dt_from
- print("{}:  ---> Days Range: {}({}) - {}({})".format(getDT(), s_dt_from, lowLimit, s_dt_to, upperLimit) )
- lowLimit=upperLimit+1
- upperLimit=lowLimit+increments
+
+# ----iterate/loop between dates (old date to current)
+print("------ DATES: Current to backwards ------")
+increments = int(30)
+s_dt_oldest = '2020-01-01'
+
+lowLimit = 1
+upperLimit = lowLimit + increments
+s_dt_from = (date.today() - timedelta(upperLimit)).strftime('%Y-%m-%d')
+
+while s_dt_from > s_dt_oldest:
+    s_dt_from = (date.today() - timedelta(upperLimit)).strftime('%Y-%m-%d')
+    s_dt_to = (date.today() - timedelta(lowLimit)).strftime('%Y-%m-%d')
+    s_dt_from = s_dt_oldest if s_dt_oldest > s_dt_from else s_dt_from
+    print(f"{getDT()}:  ---> Days Range: {s_dt_to}({upperLimit}) - {s_dt_from}({lowLimit})")
+    lowLimit = upperLimit + 1
+    upperLimit = upperLimit + increments
+
+
  
