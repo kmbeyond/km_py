a588eced46bdb0c9e5817145892d9c3eeff5c5ba
kmbeyond <kiran.learn7@gmail.com>
2022-04-12 12:34:30 -0400

    Update list_dir_contents.py

--- list_dir_contents.py
+++ list_dir_contents.py
@@ -1,75 +1,60 @@
 
 import os
-
 #os Functions
 #https://docs.python.org/2/library/os.html
 
+curr_dir = os.getcwd()
+print(f"cwd: {curr_dir}")
+
+
 #list of files in current dir
 #from os import listdir
-print(f"Listdir: {os.listdir(os.getcwd())}")
+files_list = os.listdir(os.getcwd())
+print(f"Listdir: {files_list}")
 
 
-#Print directories, subdirectories & files
-def print_dir_iter_contents(sPath):
+#Get directories, subdirectories & files
+def get_dir_contents(s_path, files_list, add_dir_flag):
     import os
-    for sChild in os.listdir(sPath):
-        sChildPath = os.path.join(sPath,sChild)
-        if os.path.isdir(sChildPath):
-            list_dir_contents(sChildPath)
+    for s_item in os.listdir(s_path):
+        s_item = os.path.join(s_path, s_item)
+        if os.path.isdir(s_item):
+            if add_dir_flag: files_list.append(f'd - {s_item}')
+            get_dir_contents(s_item, files_list, add_dir_flag)
         else:
-            print("print_dir_iter_contents=", sChildPath)
+            files_list.append(f'f - {s_item}')
 
-def list_dir_contents(sPath):
-    from os import listdir
-    dirContents = [f for f in listdir(sPath)]
-    return dirContents
-
-def list_files(sPath):
-    from os import listdir
-    from os.path import isfile, join
-    onlyfiles = [f for f in listdir(sPath) if isfile(join(sPath, f))]
-    return onlyfiles
+files_list = []
+#get_dir_contents(os.getcwd(), files_list, add_dir_flag=True)
+for file in files_list:
+    print(f" ---> {file}")
 
 
-#Print files only
-def print_files(sPath):
+
+
+#---more customized - show only path after input path
+def get_dir_contents_2(s_path, files_list, add_dir_flag, show_full_path):
     import os
-    for sChild in os.listdir(sPath):
-        sChildPath = os.path.join(sPath,sChild)
-        print(sChildPath)
-        if os.access(sChildPath, os.R_OK):
-            try:
-                with open(sChildPath) as fp:
-                    print(fp.read())
-            except IOError as e:
-                if e.errno == errno.EACCES:
-                    return "some default data"
-                # Not a permission error.
-                raise
-            else:
-                with fp:
-                    return fp.read()
+    for s_item in os.listdir(s_path):
+        s_item = os.path.join(s_path, s_item)
+        if os.path.isdir(s_item):
+            if add_dir_flag: files_list.append(f'd - {s_item}'.replace(show_full_path, ''))
+            get_dir_contents_2(s_item, files_list, add_dir_flag, show_full_path)
+        else:
+            files_list.append(f'f - {s_item}'.replace(show_full_path, ''))
+
+#view dir, files & inside sub-dir
+files_list2 = []
+#get_dir_contents_2(os.getcwd(), files_list2, add_dir_flag=True, show_full_path=os.getcwd())
+for file in files_list2:
+    print(f" ---> {file}")
 
 
-print("cwd: {}".format(os.getcwd()))
-#print("terminal: {}".format(os.ctermid()))
+#view only files (files inside subdir)
+files_only_list2 = []
+get_dir_contents_2(os.getcwd(), files_only_list2, add_dir_flag=False, show_full_path=os.getcwd())
+for file in files_only_list2:
+    print(f" ---> {file}")
 
 
-files = list_files("/home/kiran")
-#files = list_dir_contents("C:\\")
-for f in files:
-    print(f)
 
-#print("-------------listdir-----------")
-#print(os.listdir("/home/kiran"))
-
-print_dir_iter_contents("/home/kiran")
-#print_files("/home/kiran")
-
-
-#print("login user: {}".format(os.getlogin()))
-#print("PATH: {}".format(os.getenv("PATH")))
-
-
-for k in sorted(dictEmp):
-    print("Key={}; val={}".format(k, dictEmp[k]))
