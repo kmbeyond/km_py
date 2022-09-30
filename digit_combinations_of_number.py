57f88129c78216de659f6eb049021031124052a1
kmbeyond <kiran.learn7@gmail.com>
2022-09-30 14:59:28 -0400

    Update digit_combinations_of_number.py

--- digit_combinations_of_number.py
+++ digit_combinations_of_number.py
@@ -2,15 +2,46 @@
 mynum=427
 mylist = list(str(mynum))
 
-#-----2 digits
-print("2 digit combinations:")
+print(f"input: {mylist}")
 
+print("----itertools-----")
 import itertools
-combination_list = list(itertools.product(mylist,repeat=2))
-print(sorted([int(''.join([str(i) for i in list(i)])) for i in combination_list]))
-#=>gives duplications like 44,22,77
+
+print("itertools.combinations()")
+print("-- 2 digits")
+comb_list = list(itertools.combinations(mylist, 2))
+comb_list2 = [i for i in comb_list if i[0]!=i[1]]
+print(comb_list2)
+print(sorted([int(''.join([str(i) for i in list(i)])) for i in comb_list2]))
+
+print("-- 3 digits")
+comb_list = list(itertools.combinations(mylist, 3))
+comb_list3 = [i for i in comb_list if i[0]!=i[1] and i[2]!=i[1] and i[2]!=i[0]]
+print(comb_list3)
+print(sorted([int(''.join([str(i) for i in list(i)])) for i in comb_list3]))
+
+
+print("itertools.product()")
+print("-- 2 digits")
+comb_list = list(itertools.product(mylist, repeat=2))
+comb_list2 = [i for i in comb_list if i[0]!=i[1]]
+print(comb_list2)
+print(sorted([int(''.join([str(i) for i in list(i)])) for i in comb_list2]))
+
+print("-- 3 digits")
+comb_list = list(itertools.product(mylist, repeat=3))
+comb_list3 = [i for i in comb_list if i[0]!=i[1] and i[2]!=i[1] and i[2]!=i[0]]
+print(comb_list3)
+print(sorted([int(''.join([str(i) for i in list(i)])) for i in comb_list3]))
+
+
 
 #----manually
+print("manual")
+
+
+print("2 digit combinations:")
+
 def combinations_2(s):
     new_list=[]
     for i in range(len(s)-1):
@@ -27,11 +58,7 @@ print(sorted([int(''.join([str(i) for i in list(i)])) for i in combination_list]
 #-----3 digits
 print("3 digit combinations:")
 
-import itertools
-combination_list = list(itertools.product(mylist,repeat=3))
-print(sorted([int(''.join([str(i) for i in list(i)])) for i in combination_list]))
-#=>gives duplications like 444, 442, 422,...
-
+print("manual")
 #----manual
 def combinations_3(s):
     new_list=[]
@@ -49,3 +76,6 @@ def combinations_3(s):
 combination_list = list(combinations_3(mylist))
 
 print(sorted([int(''.join([str(i) for i in list(i)])) for i in combination_list]))
+
+
+#-------
