d44dd44cd120e0eeae44a0e44185febed642bca8
kmbeyond <kiran.learn7@gmail.com>
2022-10-01 14:29:29 -0500

    Update digit_swap_results_fall_within_input_range.py

--- digit_swap_results_fall_within_input_range.py
+++ digit_swap_results_fall_within_input_range.py
@@ -16,9 +16,11 @@ def swap_digits(mynum):
         for j in range(i+1,len(mylist)):
             #yield (i,j)
             mylist_temp=mylist.copy()  #mylist_temp=mylist[:]
-            temp=mylist_temp[i]
-            mylist_temp[i]=mylist_temp[j]
-            mylist_temp[j]=temp
+            #swap elements
+            mylist_temp[i],mylist_temp[j] = mylist_temp[j], mylist_temp[i]
+            #temp=mylist_temp[i]
+            #mylist_temp[i]=mylist_temp[j]
+            #mylist_temp[j]=temp
             print(f"{mylist}: {i}-{j} => {mylist_temp}")
             yield int("".join(mylist_temp))
 
@@ -35,9 +37,11 @@ def swap_digits_using_bytes(mynum):
     for i in range(len(myba)-1):
         for j in range(i+1,len(myba)):
             myba_temp=myba.copy()  #myba_temp=myba[:]
-            temp=myba_temp[i]
-            myba_temp[i]=myba_temp[j]
-            myba_temp[j]=temp
+            #swap elements
+            myba_temp[i],myba_temp[j]=myba_temp[j],myba_temp[i]
+            #temp=myba_temp[i]
+            #myba_temp[i]=myba_temp[j]
+            #myba_temp[j]=temp
             print(f"{myba.decode()}: {i}-{j} => {myba_temp.decode()}")
             yield int(myba_temp.decode())
 
@@ -47,7 +51,6 @@ print(list(swap_digits_using_bytes(mynum)))
 #427: 0-2 => 724
 #427: 1-2 => 472
 #[247, 724, 472]
-
 
 
 def check_in_limits_after_swap(num_to_check, min1, max1):
@@ -62,5 +65,30 @@ print ( check_in_limits_after_swap(427, 700,800) )
 print ( check_in_limits_after_swap(427, 900,1000) )
 
 
+
+#-----Solution with minimal code
+
+#return copy of swapped list (DONT SWAP IN ORIGINAL LIST)
+def my_swap(mylist, i ,j):
+ mylist_temp=mylist.copy()
+ mylist_temp[i],mylist_temp[j] = mylist_temp[j], mylist_temp[i]
+ return mylist_temp
+
+#get swapped numbers list
+def get_swapped_nums_list(mynum):
+    mylist = list(str(mynum))
+    return [int("".join(my_swap(mylist,i,j))) for i in range(len(mylist)-1) for j in range(i+1,len(mylist)) ]
+
+def check_in_limits_after_swap(num_to_check, min1, max1):
+    all_matches = list(get_swapped_nums_list(num_to_check))
+    for i in all_matches:
+        if i>min1 and i<max1:
+            return True
+    return False
+
+check_in_limits_after_swap(427, 700,800)
+check_in_limits_after_swap(427, 900,1000)
+
+#------------
 
 
