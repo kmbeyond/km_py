17b68901400e71144b4400c76a3e796614298cb2
kmbeyond <kiran.learn7@gmail.com>
2022-10-13 19:58:10 -0400

    Update list_items_group_into_buckets.py

--- list_items_group_into_buckets.py
+++ list_items_group_into_buckets.py
@@ -1,7 +1,7 @@
 
 #put items into bucket & get count in each bucket
 
-items_list = [2,5,8,9,11,13,15,16,18,19]
+items_list = [2,5,8,9,11,13,15,16,18,19,26]
 bucket_size = 5
 
 #Scenario#1: without given bucket boundaries
@@ -11,10 +11,16 @@ items_rounded = [int(i/bucket_size)*bucket_size for i in items_list]
 print(items_rounded)
 
 items_grp_counts = {}
-for i in items_rounded: items_grp_counts[str(i)+'-'+str(i+5)]=items_grp_counts.get(str(i)+'-'+str(i+5),0)+1
+for i in items_rounded: items_grp_counts[i]=items_grp_counts.get(i,0)+1
+#for i in items_rounded: items_grp_counts[str(i)+'-'+str(i+5)]=items_grp_counts.get(str(i)+'-'+str(i+5),0)+1
 
 print(items_grp_counts)
-#=> {'0-5': 1, '5-10': 3, '10-15': 2, '15-20': 4}
+#=> {0: 1, 5: 3, 10: 2, 15: 4, 25: 1}
+
+items_bucket_counts = {}
+items_bucket_counts = dict([(str(k)+'-'+str(k+bucket_size), v) for k,v in items_grp_counts.items()])
+print(items_bucket_counts)
+#=> {'0-5': 1, '5-10': 3, '10-15': 2, '15-20': 4, '25-30': 1}
 
 
 #Scenario#2: with bucket boundaries, so put 0 in bucket if not existing
