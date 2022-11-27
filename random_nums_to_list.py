e4f3da20d9ef2816842a7996d1bde34c6372a393
Kiran M <kiran.learn7@gmail.com>
2022-11-27 09:15:41 -0600

    KM

--- random_nums_to_list.py
+++ random_nums_to_list.py
@@ -5,40 +5,41 @@ def gen_random_item_from_list(list_lookup):
     return list_lookup[random.randint(0, len(list_lookup) - 1)]
 
 #overloaded function to check to get unique item
-def gen_random_item_from_list(list_lookup, add_to_list, retries=3):
-    import random
+def gen_random_item_from_list2(list_lookup, add_to_list, retries):
     # Does not return duplicates
     while retries > 0:
         rand_item = gen_random_item_from_list(list_lookup)
+        #print('random item:', rand_item)
         if rand_item in add_to_list:
             retries -= 1
         else:
-            #add_to_list.append(rand_item)
-            return rand_item
-    return ""
+            add_to_list.append(rand_item)
+            break #works as return
+            #return rand_item
+    #return ""
 
-def generate_random_num_list(loop_times, list_lookup, retries=3):
-    list_data=[]
+def generate_random_num_list(loop_times, list_lookup, retries):
+    list_data = []
     for i in range(loop_times):
-        get_next_item = gen_random_item_from_list(list_lookup, list_data, retries)
-        if get_next_item!='': list_data.append(get_next_item)
+        gen_random_item_from_list2(list_lookup, list_data, retries)
+        #get_next_item = gen_random_item_from_list(list_lookup, list_data, retries)
+        #if get_next_item!='': list_data.append(get_next_item)
     return list_data
 
 
 
-list_lookup = range(100000)
-loop_times = 10000
+list_lookup = list(range(1000))
+loop_times = 100
 retries = 1
 
 from datetime import datetime
+
 time_start = datetime.now()
-my_list = generate_random_num_list(loop_times, list_lookup,retries)
-print("Items count:",len(my_list))
+my_list = generate_random_num_list(loop_times, list_lookup, retries)
 time_end = datetime.now()
+print("Items count:", len(my_list))
+
 time_diff = time_end-time_start
 
 time_diff
-
-
-
 
