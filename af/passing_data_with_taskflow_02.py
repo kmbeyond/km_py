00881436afff5ed8116b139be173a831ac58fa4c
kmbeyond <kiran.learn7@gmail.com>
2023-12-26 19:15:40 -0500

    passing_data_with_taskflow_02

---
+++ af/passing_data_with_taskflow_02.py
@@ -0,0 +1,43 @@
+import time
+
+from datetime import datetime, timedelta
+from airflow.utils.dates import days_ago
+from airflow.decorators import dag, task
+
+default_args = {
+    'owner' : 'km'
+}
+
+
+
+@dag(dag_id='passing_data_with_taskflow_02',
+     description = 'Xcom using the TaskFlow API',
+     default_args = default_args,
+     start_date = days_ago(1),
+     schedule_interval = '@once',
+     tags = ['km'])
+def passing_data_with_taskflow_api():
+
+    @task
+    def get_code():
+        import random
+        return {'code': random.randint(0, 1)}
+
+    @task(multiple_outputs=True)
+    def get_status(get_code: dict):
+        get_code['status'] = 'Success' if get_code['code']==1 else 'Failure'
+        return get_code
+
+    @task
+    def display_result(code, status):
+        print(f"Return Code: {code} & {status}")
+
+
+
+    result_code = get_code()
+    result_status = get_status(result_code)
+    display_result(result_status['code'], result_status['status'])
+
+
+passing_data_with_taskflow_api()
+
