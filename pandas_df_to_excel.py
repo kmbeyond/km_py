d93fd7542b1c0d3e9f8a7b5451c7a6da92d86568
kmbeyond <kiran.learn7@gmail.com>
2024-01-10 09:33:27 -0500

    df to excel

--- pandas_df_to_excel.py
+++ pandas_df_to_excel.py
@@ -1,18 +1,22 @@
 
 ---------Write Pandas dataframe to Excel
---using any of these 2 engines
-pip install openpyxl
-pip install xlsxwriter
+#--using ANY of these 2 engines
+#pip install openpyxl
+#pip install xlsxwriter
 
-import xlsxwriter
 import pandas as pd
 data = [(1, 'aa', '2020-01-01'), (2, 'bb', '2021-01-01'), (3, 'cc', '2021-12-01')]
 dataDF = pd.DataFrame(data, columns=['id', 'name', 'login_date'])
-dataDF.to_excel('C:\km\km_test_xlsxwriter.xlsx', 'report', engine='xlsxwriter', index=False)
 
---using pandas excelwriter
+import openpyxl
+dataDF.to_excel('C:\km\km_test_openpyxl2.xlsx', 'report', engine='openpyxl', index=False)
+
+import xlsxwriter
+dataDF.to_excel('C:\km\km_test_xlsxwriter2.xlsx', 'report', engine='xlsxwriter', index=False)
+
+
+#--using pandas excelwriter : REQUIRES any of above engines
 from pandas.io.excel._base import ExcelWriter
 writer = pd.ExcelWriter('C:\km\km_test_xlsxwriter2.xlsx', engine='xlsxwriter')
 dataDF.to_excel(writer, sheet_name='Sheet1')
 writer.close()
-
