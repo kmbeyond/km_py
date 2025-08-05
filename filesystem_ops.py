e7d8a5e3ee6df613d626cfbea557642267797a36
kmbeyond <kiran.learn7@gmail.com>
2025-08-05 17:29:16 -0500

    filesystem_ops

--- filesystem_ops.py
+++ filesystem_ops.py
@@ -4,3 +4,11 @@ from pathlib import Path
 for itm in Path.iterdir(Path.cwd()):
     print(f"File:{itm}")
 
+search_path = '/Users/km/km_practice/data'
+
+for itm in os.listdir(search_path):
+    print(f"File:{itm}")
+
+files_list = [file_name for file_name in os.listdir(search_path) if 'KM_DATA' in file_name]
+
+len(files_list)
